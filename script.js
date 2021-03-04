76f077a609b1ff095e77abf47881eef0c9b60e6f
JJ <jonathanjablonski94@gmail.com>
2021-03-04 12:42:57 -0600

    Created questions.js, script.js currently displays question and answers, does not return a wrong/right value though

---
+++ script.js
@@ -0,0 +1,58 @@
+let timeRemaining = 60;
+let startBtn = document.querySelector('#start');
+let timeInterval;
+let pageTime = document.querySelector('#time');
+let question = document.querySelector('#question');
+let name = document.querySelector('#name');
+let choices = document.querySelector('#choices');
+let currentQuestion;
+let counter = 0;
+
+// Starting quiz
+function startGame(){
+    timeInterval = setInterval(timeRun, 1000);
+
+    displayQuestion();
+}
+
+// Show questions in the browser
+function displayQuestion() {
+    let currentQuestion = questions[0];
+
+    name.textContent = currentQuestion.name;
+
+    currentQuestion.choices.forEach(function(choice){
+        let choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("value", choice);
+        choiceBtn.addEventListener("click", questionClick);
+        choiceBtn.textContent = choice;
+
+        choices.appendChild(choiceBtn);
+    });
+}
+
+// Upon answering the question
+function answerClick(e){
+    if(e.target.value === currentQuestion.answer){
+        console.log('correct');
+    } else {
+        console.log('false');
+    }
+
+    counter++
+
+    displayQuestion();
+}
+
+// Timer
+function timeRun() {
+    timeRemaining--;
+    pageTime.textContent = timeRemaining;
+
+    if (timeRemaining === 0) {
+        clearInterval(timeInterval);
+    }
+}
+
+startBtn.addEventListener('click', startGame);
+
---
+++ starting-workflow.md
@@ -0,0 +1,19 @@
+// Create A JS Variable That
+    - Tracks what question your user is on
+    - Stores all of the questions (array of objects)
+    - Stores the high scores
+    - Stores time left to answer
+    - Stores the user's name
+    - Stores the user's score (number)
+    - Stores the users answers (array)
+// Create an HTML Element That:
+    - Shows High Scores (Div)
+    - Lets user enter their name (Input)
+    - Displays the user's name
+    - Displays their score
+    - Displays the question they need to answer
+    - Displays the possible answers to the question
+    - Displays time left for a user
+    - Button to start quiz
+    - holds a paragraph of text for the rules
+    - Displays the end game screen
