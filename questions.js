d1eb1a0c0a122bee433e33051e37d8102038d67d
JJ <jonathanjablonski94@gmail.com>
2021-03-06 21:53:05 -0600

    More files

---
+++ questions.js
@@ -0,0 +1,23 @@
+let questions = [
+    {
+        name: "Commonly used data types DO NOT include:",
+        answer: 'Alerts',
+        choices: ["Strings","Boolean","Arrays","Alerts"],
+    },
+    {
+        name: "The conditional statement 'if/else' is enclosed with these:",
+        answer: "Curly brackets",
+        choices: ["Curly brackets","Parentheses","Quotations","Backslashes"],
+    },
+    {
+        name: "It is highly recommended to use this tool for debugging:",
+        answer: "DevTools",
+        choices: ["Notepad","DevTools","Editor","Terminal"],
+    },
+    {
+        name: "Arrays in JavaScript can be stored in:",
+        answer: "All of the above",
+        choices: ["Separate .js files","Variables","Functions","All of the above"],
+    },
+    
+];
---
+++ script.js
@@ -0,0 +1,126 @@
+let timeRemaining = 60;
+let startBtn = document.querySelector('#start');
+let timeInterval;
+let pageTime = document.querySelector('#time');
+let question = document.querySelector('#question');
+let questionName = document.querySelector('#name');
+let choices = document.querySelector('#choices');
+let currentQuestion;
+let counter = 0;
+let userName = document.querySelector('#userName');
+let nameScore = document.querySelector('#score');
+let scoreName = document.querySelector('#scoreName');
+let scoreSubmit = document.querySelector('#scoreSubmit');
+let greetingMsg = document.querySelector('#greetingMsg');
+let rightAnswer = document.querySelector('#rightAnswer');
+let wrongAnswer = document.querySelector('#wrongAnswer');
+let highScores = [];
+
+scoreSubmit.style.visibility = "hidden";
+nameScore.style.visibility = "hidden";
+userName.style.visibility = "hidden";
+
+
+// Starting quiz
+function startGame(){
+    timeInterval = setInterval(timeRun, 1000);
+    
+    displayQuestion();
+}
+
+// Show questions in the browser
+function displayQuestion() {
+    currentQuestion = questions[counter];
+    greetingMsg.style.visibility = "hidden";
+    question.textContent = currentQuestion.name;
+
+    currentQuestion.choices.forEach(function(choice){
+        let choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("value", choice);
+        choiceBtn.addEventListener("click", answerClick);
+        choiceBtn.textContent = choice;
+
+        choices.appendChild(choiceBtn);
+        
+        startBtn.style.visibility = "hidden";
+    });
+}
+
+// Upon answering the question
+function answerClick(e){
+    e.preventDefault();
+    console.log(currentQuestion);
+    if(e.target.value === currentQuestion.answer){
+        console.log('true');
+        
+    } else {
+        console.log('false');
+        timeRemaining = timeRemaining - 10;
+    }
+
+    counter++
+
+    if(counter < questions.length){
+        choices.innerHTML = '';
+        displayQuestion();
+    } else{
+        alert('Game Over');
+        endGame();
+    }
+}
+
+    
+
+// End of the game
+function endGame() {
+    clearInterval(timeInterval);
+    nameScore.removeAttribute('class');
+    choices.style.visibility = "hidden";
+    question.style.visibility = "hidden";
+    scoreSubmit.style.visibility = "visible";
+    nameScore.style.visibility = "visible";
+    userName.style.visibility = "visible";
+    
+
+}
+
+// Gathering scores and initials for high scores
+scoreSubmit.addEventListener("click", function(event){
+    event.preventDefault();
+    highScores.push({
+        name: userName.value,
+        score: timeRemaining
+    });
+    localStorage.setItem('scores',JSON.stringify(highScores));
+});
+
+// Timer
+function timeRun() {
+    timeRemaining--;
+    pageTime.textContent = `Time ${timeRemaining}`;
+
+    if (timeRemaining <= 0) {
+        clearInterval(timeInterval);
+    }
+    
+}
+
+// Appending high scores to an array for display
+function renderScores() {
+    const highScoreDiv = document.querySelector('#high-scores');
+    if (localStorage.getItem('scores')) {
+        highScores = JSON.parse(localStorage.getItem('scores'));
+        if (highScores.length > 0) {
+            for(let i = 0; i < highScores.length; i++) {
+                const div = document.createElement('div');
+                div.textContent = `${highScores[i].name} - ${highScores[i].score}`;
+                highScoreDiv.appendChild(div);
+            }
+        }
+    }
+    document.getElementById('view-scores').addEventListener('click', renderScores);
+}
+renderScores();
+
+
+startBtn.addEventListener("click", startGame);
